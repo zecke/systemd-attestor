81c80042bc85ede2b02939d760aebc9c3c8c32ed
Holger Hans Peter Freyther <automatic@freyther.de>
2021-01-03 00:03:55 +0800

    Add a basic systemd workload attestor plugin
    Resolve the unit based on the PID. Use the fully qualified fragment
    path as a unique attribute. Start by using the d-bus system bus to
    get information. Most likely this will be one the first options to
    change between system, user and direct connection to systemd.
    
    Signed-off-by: Holger Hans Peter Freyther <automatic@freyther.de>

--- plugin.go
+++ plugin.go
@@ -1,78 +1,84 @@
 package main
 
-import(
-	"github.com/hashicorp/go-plugin"
-	spi "github.com/spiffe/spire/proto/common/plugin"
+import (
+	"context"
+	"fmt"
 
-	//import appropriate package corresponding to the plugin type
-	//"github.com/spiffe/spire/proto/server/nodeattestor"
-	//"github.com/spiffe/spire/proto/server/noderesolver"
-	//"github.com/spiffe/spire/proto/server/datastore"
-	//"github.com/spiffe/spire/proto/server/ca"
-	//"github.com/spiffe/spire/proto/server/datastore"
-	//"github.com/spiffe/spire/proto/server/upstreamca"
-
-	//"github.com/spiffe/spire/proto/agent/nodeattestor"
-	//"github.com/spiffe/spire/proto/agent/keymanager"
-	//"github.com/spiffe/node-agent/plugins/workload_attestor"
+	"github.com/godbus/dbus/v5"
+	"github.com/spiffe/spire/pkg/agent/plugin/workloadattestor"
+	"github.com/spiffe/spire/pkg/common/catalog"
+	"github.com/spiffe/spire/proto/spire/common"
+	spi "github.com/spiffe/spire/proto/spire/common/plugin"
 )
 
-const pluginName  = ""//Set PluginName"
+const pluginName = "systemd"
 
-type Plugin struct { // Rename Plugin Receiver Type
-	config1 string
+func BuiltIn() catalog.Plugin {
+	return builtin(New())
 }
 
-type PluginConfig struct { // Rename PluginConfig Type
-	Config1 string `hcl:"config_1"`
+func builtin(p *Plugin) catalog.Plugin {
+	return catalog.MakePlugin(pluginName, workloadattestor.PluginServer(p))
 }
 
+type Plugin struct {
+	workloadattestor.UnsafeWorkloadAttestorServer
+}
 
-func New() Plugin{
+func New() *Plugin {
 	//New method should return the Plugin Type being implemented
-return Plugin{}
+	return &Plugin{}
 }
 
-func (p *Plugin) Configure(*spi.ConfigureRequest) (*spi.ConfigureResponse, error) {
+func (p *Plugin) Configure(ctx context.Context, req *spi.ConfigureRequest) (*spi.ConfigureResponse, error) {
 	return &spi.ConfigureResponse{}, nil
 }
 
-func (p *Plugin) GetPluginInfo(*spi.GetPluginInfoRequest) (*spi.GetPluginInfoResponse, error) {
+func (p *Plugin) GetPluginInfo(ctx context.Context, req *spi.GetPluginInfoRequest) (*spi.GetPluginInfoResponse, error) {
 	return &spi.GetPluginInfoResponse{}, nil
 }
 
+func (p *Plugin) Attest(ctx context.Context, req *workloadattestor.AttestRequest) (*workloadattestor.AttestResponse, error) {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	// Get the unit for the given PID from the systemd service.
+	call := conn.Object("org.freedesktop.systemd1", "/org/freedesktop/systemd1").CallWithContext(ctx, "org.freedesktop.systemd1.Manager.GetUnitByPID", 0, uint(req.Pid))
+	var unitPath dbus.ObjectPath
+	if err := call.Store(&unitPath); err != nil {
+		return nil, err
+	}
+
+	var selectors []*common.Selector
+
+	// Get the location of the service file
+	fragmentPathVariant, err := conn.Object("org.freedesktop.systemd1", unitPath).GetProperty("org.freedesktop.systemd1.Unit.FragmentPath")
+	if err != nil {
+		return nil, err
+	}
+	fragmentPath, ok := fragmentPathVariant.Value().(string)
+	if !ok {
+		return nil, fmt.Errorf("Returned fragment path was not a string: %v", fragmentPathVariant.String())
+	}
+	selectors = append(selectors, makeSelector("fragmentPath", fragmentPath))
+
+	// TODO(zecke): Add other interesting bits of the unit.
+
+	return &workloadattestor.AttestResponse{
+		Selectors: selectors,
+	}, nil
+}
+
+func makeSelector(kind, value string) *common.Selector {
+	return &common.Selector{
+		Type:  pluginName,
+		Value: fmt.Sprintf("%s:%s", kind, value),
+	}
+}
+
 func main() {
-	plugin.Serve(&plugin.ServeConfig{
-
-		Plugins: map[string]plugin.Plugin{
-			//Uncomment one of the lines below corresponding to the plugin type being implemented
-
-			//SPIRE Server Plugin Type
-			//pluginName: noderesolver.NodeResolverPlugin{NodeResolverImpl: New()},
-			//pluginName: nodeattestor.NodeAttestorPlugin{NodeAttestorImpl: New()},
-			//pluginName: ca.ControlPlaneCaPlugin{ControlPlaneCaImpl: New()},
-			//pluginName: datastore.DataStorePlugin{DataStoreImpl: New()},
-			//pluginName: upstreamca.UpstreamCaPlugin{UpstreamCaImpl: New()},
-
-
-			//SPIRE Agent Plugin Type
-			//pluginName: nodeattestor.NodeAttestorPlugin{NodeAttestorImpl: New()},
-			//pluginName: keymanager.KeyManagerPlugin{KeyManagerImpl: New()},
-			//pluginName: workloadattestor.WorkloadAttestorPlugin{WorkloadAttestorImpl: New()},
-
-		},
-		//Uncomment the line corresponding to the plugin type
-		//HandshakeConfig: nodeattestor.Handshake,
-		//HandshakeConfig: noderesolver.Handshake,
-		//HandshakeConfig: ca.Handshake,
-		//HandshakeConfig: datastore.Handshake,
-		//HandshakeConfig: upstreamca.Handshake,
-
-		//HandshakeConfig: nodeattestor.Handshake,
-		//HandshakeConfig: keymanager.Handshake,
-		//HandshakeConfig: workloadattestor.Handshake,
-
-		GRPCServer: plugin.DefaultGRPCServer,
-	})
-
+	catalog.PluginMain(BuiltIn())
 }
